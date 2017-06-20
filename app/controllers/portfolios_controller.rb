ab4f6a3ac9560b582d6675202cccb88dffd08383
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-06-20 16:04:14 +0000

    Implemented edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -11,7 +11,7 @@ class PortfoliosController < ApplicationController
         @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
       
       respond_to do |format|
-      if @portfolio_item.save
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'Portfolio item is now live.' }
       else
         format.html { render :new }
@@ -19,5 +19,20 @@ class PortfoliosController < ApplicationController
   end
      end
     
-   
+def edit
+    @portfolio_item = Portfolio.find(params[:id])
 end
+
+def update
+   @portfolio_item = Portfolio.find(params[:id]) 
+   
+      respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item was updated.' }
+      else
+        format.html { render :edit }
+      end
+  end
+
+end
+end 
